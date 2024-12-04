b97c5e540d49ea92d5ec3557f1d4cd6a9ff38479
Kislay Verma <kislayuv27@gmail.com>
2024-12-04 14:01:45 +0530

    type exec

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::{env, fs};
 
 fn main() {
     // start repl
@@ -27,6 +28,8 @@ fn repl() {
             } else {
                 if is_builtin(toks[1]) {
                     println!("{} is a shell builtin", toks[1]);
+                } else if let Some(addr) = find_in_paths(toks[1]) {
+                    println!("{} is {}", toks[1], addr);
                 } else {
                     println!("{}: not found", toks[1]);
                 }
@@ -44,6 +47,45 @@ fn is_builtin(s: &str) -> bool {
     match s {
         "exit" | "echo" | "type" => true,
         _ => false,
+    }
+}
+
+fn find_in_paths(s: &str) -> Option<String> {
+    match env::var("PATH") {
+        Ok(value) => {
+            let paths = value.split(":").collect::<Vec<&str>>();
+            for path in paths {
+                if path_contains_file(path, s) {
+                    let ret = format!("{}/{}", path, s);
+                    return Some(ret);
+                }
+            }
+            None
+        }
+        Err(_e) => None,
+    }
+}
+
+fn path_contains_file(path: &str, s: &str) -> bool {
+    match fs::read_dir(path) {
+        Ok(value) => {
+            for entry in value {
+                if !entry.is_ok() {
+                    continue;
+                }
+                let entry: fs::DirEntry = entry.unwrap();
+                if let Some(fname) = entry.file_name().to_str() {
+                    if fname == s {
+                        return true;
+                    }
+                }
+            }
+            false
+        }
+        Err(e) => {
+            eprintln!("Could not read dir {}: {}", path, e);
+            false
+        }
     }
 }
 
