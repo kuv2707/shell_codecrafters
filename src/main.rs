0ae2fa197a286f919523610cf15828df6892f990
Kislay Verma <kislayuv27@gmail.com>
2024-12-04 13:26:23 +0530

    echo

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,24 @@ fn main() {
 
 fn repl() {
     print!("$ ");
-    io::stdout().flush().unwrap();
+    flushio();
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
     let toks = input.trim().split_whitespace().collect::<Vec<&str>>();
     match toks[0] {
         "exit" => std::process::exit(0),
+        "echo" => {
+            println!("{}", toks[1..].join(" "));
+            flushio();
+        }
         _ => {
             println!("{}: command not found", input.trim());
-            io::stdout().flush().unwrap();
+            flushio();
         }
     }
+}
+
+fn flushio() {
+    io::stdout().flush().unwrap();
 }
